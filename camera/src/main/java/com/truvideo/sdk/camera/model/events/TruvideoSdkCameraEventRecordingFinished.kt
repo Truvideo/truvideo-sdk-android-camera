ee6fe50bb8542cb237fc5b502ea6121e9c7a023b
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-11-15 19:09:24 +0000

    Update 76.2.5-beta.115

---
+++ camera/src/main/java/com/truvideo/sdk/camera/model/events/TruvideoSdkCameraEventRecordingFinished.kt
@@ -0,0 +1,22 @@
+package com.truvideo.sdk.camera.model.events
+
+import com.truvideo.sdk.camera.model.TruvideoSdkCameraMedia
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class TruvideoSdkCameraEventRecordingFinished(
+    val media: TruvideoSdkCameraMedia,
+) : TruvideoSdkCameraEventData() {
+    fun toJson(): String = Json.encodeToString(this)
+
+    companion object {
+        fun fromJson(json: String): TruvideoSdkCameraEventRecordingFinished {
+            val jsonConfig = Json {
+                ignoreUnknownKeys = true
+            }
+            return jsonConfig.decodeFromString(json)
+        }
+    }
+}
