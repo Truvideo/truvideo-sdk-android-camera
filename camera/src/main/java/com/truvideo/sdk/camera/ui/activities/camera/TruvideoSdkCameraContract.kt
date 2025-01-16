9ffe57f891041c92cdc96122559f88b2a4c3ebab
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2025-01-16 18:59:35 +0000

    Update 76.3.3-rc.121

---
+++ camera/src/main/java/com/truvideo/sdk/camera/ui/activities/camera/TruvideoSdkCameraContract.kt
@@ -0,0 +1,35 @@
+package com.truvideo.sdk.camera.ui.activities.camera
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import com.truvideo.sdk.camera.model.TruvideoSdkCameraConfiguration
+import com.truvideo.sdk.camera.model.TruvideoSdkCameraMedia
+
+
+class TruvideoSdkCameraContract : ActivityResultContract<TruvideoSdkCameraConfiguration, List<TruvideoSdkCameraMedia>>() {
+
+    override fun createIntent(context: Context, input: TruvideoSdkCameraConfiguration): Intent {
+        return Intent(context, TruvideoSdkCameraActivity::class.java).apply {
+            putExtra("configuration", input.toJson())
+        }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): List<TruvideoSdkCameraMedia> {
+        return when (resultCode) {
+            Activity.RESULT_OK -> {
+                val data: ArrayList<String> = intent?.getStringArrayListExtra("media") ?: arrayListOf()
+                data.mapNotNull { json ->
+                    try {
+                        TruvideoSdkCameraMedia.fromJson(json)
+                    } catch (e: Exception) {
+                        null
+                    }
+                }.toList()
+            }
+
+            else -> emptyList()
+        }
+    }
+}
