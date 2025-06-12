f1480b15908ae9d2698eba7248b5d4bdd890703a
Bitrise CI <noreply@bitrise.io>
2025-06-12 11:46:12 +0000

    Update 76.3.18-rc.163

---
+++ camera/src/main/java/com/truvideo/sdk/camera/service/ar/TruvideoSdkArCameraService.kt
@@ -0,0 +1,365 @@
+package com.truvideo.sdk.camera.service.ar
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.media.MediaRecorder
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import android.view.PixelCopy
+import com.truvideo.sdk.camera.model.TruvideoSdkCameraInformation
+import com.truvideo.sdk.camera.model.TruvideoSdkCameraOrientation
+import com.truvideo.sdk.camera.model.TruvideoSdkCameraResolution
+import com.truvideo.sdk.camera.service.camera.TruvideoSdkCameraService
+import com.truvideo.sdk.camera.utils.PausableTimer
+import com.truvideo.sdk.camera.utils.createFile
+import com.truvideo.sdk.camera.utils.deleteFile
+import com.truvideo.sdk.camera.utils.getVideoDuration
+import com.truvideo.sdk.camera.utils.save
+import io.github.sceneview.ar.ARSceneView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import truvideo.sdk.common.exceptions.TruvideoSdkException
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+internal class TruvideoSdkArCameraService(
+    private val context: Context,
+    private val information: TruvideoSdkCameraInformation?,
+    private val serviceCallback: TruvideoSdkArCameraServiceCallback,
+) {
+
+    private var isRecording: Boolean = false
+    private var isBusy: Boolean = false
+    private var isPaused: Boolean = false
+
+    companion object {
+        const val TAG: String = "[TruvideoSdkCamera][TruvideoSdkArCameraService]"
+    }
+
+    var maxDuration: Int? = null
+    private var maxDurationReported = false
+    private var videoDurationTimer: PausableTimer? = null
+
+    lateinit var outputPath: String
+    private var mediaRecorder: MediaRecorder? = null
+    private var backgroundHandler: Handler? = null
+
+    var videoDuration = MutableStateFlow(0L)
+
+    private lateinit var videoFile: File
+    private lateinit var sceneView: ARSceneView
+
+    val videoSize: Size
+        get() {
+            val backCamera = information?.backCamera ?: throw TruvideoSdkException("No back camera found")
+            val resolution = backCamera.resolutions.first()
+            return Size(
+                resolution.width,
+                resolution.height
+            )
+        }
+
+    init {
+        val name = "ArCameraServiceThread-${System.currentTimeMillis()}"
+        val thread = HandlerThread(name)
+        thread.start()
+        backgroundHandler = Handler(thread.looper)
+    }
+
+    fun setUpSceneView(sceneView: ARSceneView) {
+        this.sceneView = sceneView
+    }
+
+    private fun startTimer() {
+        stopTimer()
+
+        videoDurationTimer = PausableTimer(
+            onUpdate = { videoDuration.value = it },
+        )
+        videoDurationTimer?.start()
+    }
+
+    private fun stopTimer() {
+        videoDurationTimer?.stop()
+        videoDurationTimer = null
+        videoDuration.value = 0L
+    }
+
+    fun toggleRecording(maxVideoDurationReached: Boolean = false) {
+        if (isBusy) return
+        updateIsBusy(true)
+        scope.launch {
+            if (isRecording) {
+                videoDurationTimer?.pause()
+                updateIsRecording(false)
+                stopMediaRecording(maxVideoDurationReached)
+                updateIsBusy(false)
+            } else {
+                updateIsRecording(true)
+                startRecording()
+            }
+        }
+    }
+
+    private fun updateIsRecording(value: Boolean) {
+        isRecording = value
+        serviceCallback.updateIsRecording(value)
+    }
+
+    private fun updateIsPaused(value: Boolean) {
+        isPaused = value
+        serviceCallback.updateIsPaused(value)
+    }
+
+    private fun updateIsBusy(value: Boolean) {
+        isBusy = value
+        serviceCallback.updateIsBusy(value)
+    }
+
+    private fun startRecording(callback: (() -> Unit)? = null) {
+        setupMediaRecorder()
+        val m = mediaRecorder ?: throw RuntimeException("Media recorder not found")
+        val recordingSurface = m.surface
+
+        try {
+            sceneView.startMirroring(
+                recordingSurface,
+                0,
+                0,
+                videoSize.height,
+                videoSize.width
+            )
+        } catch (e: IOException) {
+            Log.e("TruvideoSdkArCameraService", "Exception setting up recorder", e)
+            updateIsBusy(false)
+            updateIsRecording(false)
+            if (callback != null) callback()
+            return
+        }
+
+        Log.d(TAG, "Camera recording started")
+        updateIsBusy(false)
+        serviceCallback.onRecordingStarted()
+//        m.start()
+        startTimer()
+        if (callback != null) callback()
+
+        Log.d(TAG, "Media recording created. Path ${videoFile.path}")
+
+    }
+
+    private fun createVideoFile(): File? {
+        val name = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_SSS", Locale.US).format(Date())
+        return createFile(outputPath, name, "mp4")
+    }
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    private fun setupMediaRecorder() {
+//        stopMediaRecording()
+
+        val newFile = createVideoFile() ?: throw RuntimeException("Error creating video file")
+
+        videoFile = newFile
+
+        val rotation = serviceCallback.getSensorRotation()
+
+        val m = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(context)
+        } else {
+            @Suppress("DEPRECATION") MediaRecorder()
+        }
+        maxDurationReported = false
+        m.setOnInfoListener { _, what, _ ->
+            if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
+                scope.launch {
+                    if (!maxDurationReported) {
+                        Log.d(TAG, "Max duration reached")
+                        maxDurationReported = true
+                        toggleRecording(maxVideoDurationReached = true)
+                    }
+                }
+            }
+        }
+
+
+        m.setAudioSource(MediaRecorder.AudioSource.MIC)
+        m.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+
+        // Video Format & FPS
+        m.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        m.setVideoFrameRate(24)
+
+        // Video size, bitrate & encoder
+        m.setVideoSize(videoSize.height, videoSize.width)
+        m.setVideoEncodingBitRate(10_000_000)
+        m.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+
+        // Audio Config
+        m.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        m.setAudioSamplingRate(44100)  // 44.1 kHz
+        m.setAudioEncodingBitRate(128000)  // 128 kbps
+
+        val orientationHint = when (rotation) {
+            TruvideoSdkCameraOrientation.PORTRAIT -> 0
+            TruvideoSdkCameraOrientation.LANDSCAPE_LEFT -> 270
+            TruvideoSdkCameraOrientation.LANDSCAPE_RIGHT -> 90
+            TruvideoSdkCameraOrientation.PORTRAIT_REVERSE -> 180
+        }
+        m.setOrientationHint(orientationHint)
+        m.setOutputFile(videoFile.absolutePath)
+
+        if (maxDuration != null) {
+            m.setMaxDuration(maxDuration!!)
+        }
+
+        m.prepare()
+        mediaRecorder = m
+
+        m.start()
+
+        Log.d(TAG, "Media recording created. Path ${videoFile.path}")
+    }
+
+    private fun stopMediaRecording(maxVideoDurationReached: Boolean) {
+        val sensorRotation = serviceCallback.getSensorRotation()
+        val rotation = calculateRotation(
+            sensorRotation = 0,
+            orientation = sensorRotation.rotation
+        )
+        val resolution = when (rotation) {
+            90, 270 -> TruvideoSdkCameraResolution(videoSize.width, videoSize.width)
+            else -> TruvideoSdkCameraResolution(videoSize.height, videoSize.width)
+        }
+
+        val m = mediaRecorder
+        if (m != null) {
+            try {
+                sceneView.stopMirroring(m.surface)
+                m.stop()
+            } catch (e: Exception) {
+                Log.d(TAG, "Failed to stop media recorder", e)
+                deleteFile(videoFile)
+            }
+
+            m.reset()
+            m.release()
+            mediaRecorder = null
+
+            if (videoFile.exists()) {
+                val duration = getVideoDuration(videoFile)
+                Log.d(TAG, "Media recording stopped. Path: ${videoFile.path}. Duration: $duration")
+                serviceCallback.onVideo(
+                    file = videoFile,
+                    duration = duration,
+                    resolution = resolution,
+                    orientation = sensorRotation,
+                    maxVideoDurationReached = maxVideoDurationReached
+                )
+            } else {
+                Log.d(TAG, "No video to add")
+            }
+        }
+    }
+
+    private fun calculateRotation(
+        sensorRotation: Int,
+        orientation: Int
+    ): Int {
+        val result = sensorRotation - orientation
+        return ((result % 360) + 360) % 360
+    }
+
+    fun takeImage() {
+        updateIsBusy(true)
+        val bitmap: Bitmap = Bitmap.createBitmap(sceneView.width, sceneView.height, Bitmap.Config.ARGB_8888)
+
+        val sensorRotation = serviceCallback.getSensorRotation()
+        val rotation = calculateRotation(
+            sensorRotation = 0,
+            orientation = sensorRotation.rotation
+        )
+
+        val fixedResolution = when (rotation) {
+            90, 270 -> TruvideoSdkCameraResolution(sceneView.height, sceneView.width)
+            else -> TruvideoSdkCameraResolution(sceneView.width, sceneView.height)
+        }
+
+        PixelCopy.request(sceneView, bitmap, { copyResult ->
+            if (copyResult == PixelCopy.SUCCESS) {
+                // Save bitmap
+                val name = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss_SSS", Locale.US).format(Date())
+                bitmap.save(outputPath, name, "png", rotation) {
+                    serviceCallback.onImage(
+                        file = it,
+                        orientation = sensorRotation,
+                        resolution = fixedResolution
+                    )
+                    updateIsBusy(false)
+                }
+            } else {
+                // Error
+                updateIsBusy(false)
+            }
+        }, backgroundHandler!!)
+    }
+
+    fun disconnect(
+        stopRecording: Boolean = true,
+        callback: (() -> Unit)? = null
+    ) {
+        Log.d(TAG, "Disconnect camera")
+
+        if (stopRecording) {
+            updateIsRecording(false)
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            stopMediaRecording(false)
+            callback?.invoke()
+        }
+    }
+
+    fun pauseRecording() {
+        if (!isRecording || isPaused) {
+            Log.d(TruvideoSdkCameraService.TAG, "Recording not in progress or already paused")
+            return
+        }
+
+        videoDurationTimer?.pause()
+
+        try {
+            mediaRecorder?.pause()
+            updateIsPaused(true)
+        } catch (e: Exception) {
+            Log.e(TruvideoSdkCameraService.TAG, "Error pausing video recording", e)
+        }
+    }
+
+    fun resumeRecording() {
+        if (!isRecording || !isPaused) {
+            Log.d(TruvideoSdkCameraService.TAG, "Recording not paused or not in progress")
+            return
+        }
+
+        videoDurationTimer?.resume()
+
+        try {
+            mediaRecorder?.resume()
+            updateIsPaused(false)
+        } catch (e: Exception) {
+            Log.e(TruvideoSdkCameraService.TAG, "Error resuming video recording", e)
+        }
+    }
+
+}
+
+
---
+++ camera/src/main/java/com/truvideo/sdk/camera/ui/components/video_player/VideoPlayer.kt
@@ -0,0 +1,318 @@
+package com.truvideo.sdk.camera.ui.components.video_player
+
+import android.net.Uri
+import android.util.Log
+import android.view.TextureView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.Player
+import androidx.media3.common.VideoSize
+import androidx.media3.exoplayer.ExoPlayer
+import com.truvideo.sdk.components.TruvideoColors
+import com.truvideo.sdk.components.animated_value.animateFloat
+import com.truvideo.sdk.components.animated_value.springAnimationFloatSpec
+import com.truvideo.sdk.components.button.TruvideoIconButton
+import kotlinx.coroutines.delay
+import kotlin.math.absoluteValue
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.ZERO
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
+@Composable
+fun VideoPlayer(
+    exoPlayer: ExoPlayer? = null,
+    controlVisible: Boolean = true,
+) {
+    var videoReady by remember { mutableStateOf(false) }
+    var videoIsPlaying by remember { mutableStateOf(false) }
+    var playerPosition by remember { mutableLongStateOf(0L) }
+    var playerDuration by remember { mutableLongStateOf(0L) }
+    val sliderInteractionSource = remember { MutableInteractionSource() }
+    val isPressed by sliderInteractionSource.collectIsPressedAsState()
+    val isDragged by sliderInteractionSource.collectIsDraggedAsState()
+    val sliderIsInteracting by remember { derivedStateOf { isPressed || isDragged } }
+    var sliderTempValue by remember { mutableFloatStateOf(0f) }
+    var aspectRatio by remember { mutableFloatStateOf(1f) }
+    val playerPercentage by remember(exoPlayer, videoReady, playerDuration, sliderIsInteracting, sliderTempValue, playerPosition) {
+        derivedStateOf {
+            if (exoPlayer == null || !videoReady || playerDuration == 0L) {
+                0f
+            } else {
+                if (sliderIsInteracting) {
+                    sliderTempValue
+                } else {
+                    playerPosition.toFloat() / playerDuration.toFloat()
+                }
+            }
+        }
+    }
+
+    val controlOpacityAnim = animateFloat(
+        value = if (controlVisible) 1f else 0f
+    ).coerceIn(0f, 1f)
+    val controlYAnim = animateFloat(
+        value = if (controlVisible) 0f else 1f,
+        spec = springAnimationFloatSpec
+    )
+
+    val listener = remember {
+        object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                videoReady = true
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                videoIsPlaying = isPlaying
+            }
+
+            override fun onVideoSizeChanged(videoSize: VideoSize) {
+                super.onVideoSizeChanged(videoSize)
+                aspectRatio = videoSize.width.toFloat() / videoSize.height.toFloat()
+            }
+        }
+    }
+
+    DisposableEffect(exoPlayer) {
+        if (exoPlayer != null) {
+            exoPlayer.addListener(listener)
+
+            val state = exoPlayer.playbackState
+            videoReady = state != Player.STATE_IDLE
+
+            if (state == Player.STATE_ENDED) {
+                exoPlayer.seekTo(0L)
+                exoPlayer.pause()
+            }
+        }
+
+        onDispose {
+            exoPlayer?.removeListener(listener)
+        }
+    }
+
+    LaunchedEffect(exoPlayer) {
+        while (true) {
+            delay(50)
+            playerPosition = exoPlayer?.currentPosition ?: 0L
+            playerDuration = exoPlayer?.duration ?: 0L
+        }
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.Black)
+                .aspectRatio(aspectRatio)
+        ) {
+            if (exoPlayer != null) {
+                VideoSurface(exoPlayer)
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .graphicsLayer {
+                    alpha = controlOpacityAnim
+                    translationY = controlYAnim * size.height
+                }
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .shadow(16.dp, CircleShape)
+                    .clip(CircleShape)
+                    .background(color = Color.Black.copy(0.7f))
+                    .border(width = 2.dp, color = TruvideoColors.gray, shape = CircleShape)
+                    .padding(horizontal = 12.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    TruvideoIconButton(
+                        icon = if (videoIsPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
+                        small = true,
+                        onPressed = {
+                            val player = exoPlayer ?: return@TruvideoIconButton
+
+                            if (player.isPlaying) {
+                                player.pause()
+                            } else {
+                                val duration = player.duration
+                                val position = player.currentPosition
+                                if ((duration - position).absoluteValue < 50) {
+                                    Log.d("CameraService", "Seeking to 0")
+                                    player.seekTo(0)
+                                }
+
+                                player.play()
+                            }
+                        }
+                    )
+
+                    Box(modifier = Modifier.width(8.dp))
+
+                    Slider(
+                        modifier = Modifier.weight(1f),
+                        colors = SliderDefaults.colors(
+                            thumbColor = TruvideoColors.amber,
+                            activeTrackColor = TruvideoColors.amber,
+                        ),
+                        value = playerPercentage,
+                        onValueChange = {
+                            val player = exoPlayer ?: return@Slider
+                            val position = player.duration * it
+                            sliderTempValue = it
+
+                            if (player.isPlaying) {
+                                player.pause()
+                            }
+
+                            player.seekTo(position.toLong())
+                        },
+                        interactionSource = sliderInteractionSource
+                    )
+
+                    Box(modifier = Modifier.width(8.dp))
+
+                    Text(
+                        formatDuration(playerPosition.toInt().toDuration(DurationUnit.MILLISECONDS)),
+                        color = Color.White,
+                        fontSize = 12.sp
+                    )
+                    Box(modifier = Modifier.width(8.dp))
+
+                }
+            }
+        }
+
+    }
+}
+
+private fun formatDuration(duration: Duration): String {
+    Log.d("CameraService", "Format duration $duration ${duration.inWholeMilliseconds}")
+
+    if (duration == ZERO) return "00:00"
+
+    val hours = duration.inWholeHours
+    val minutes = (duration - hours.hours).inWholeMinutes
+    val seconds = (duration - hours.hours - minutes.minutes).inWholeSeconds
+    return if (hours > 0) {
+        "%02d:%02d:%02d".format(hours, minutes, seconds)
+    } else {
+        "%02d:%02d".format(minutes, seconds)
+    }
+}
+
+@Composable
+private fun VideoSurface(
+    player: Player
+) {
+    val context = LocalContext.current
+
+    val videoView = remember {
+        val view = TextureView(context)
+        view.tag = player
+        player.setVideoTextureView(view)
+        view
+    }
+
+    AndroidView(
+        factory = { videoView }
+    )
+    DisposableEffect(Unit) {
+        onDispose {
+            player.clearVideoTextureView(videoView)
+        }
+    }
+
+}
+
+@Composable
+@Preview
+private fun Test() {
+    var uri by remember { mutableStateOf(Uri.parse("")) }
+    var controlVisible by remember { mutableStateOf(true) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        Column {
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(0.dp))
+                    .weight(1f)
+            ) {
+                VideoPlayer(
+                    controlVisible = controlVisible,
+                )
+            }
+
+            Text("Uri: $uri", modifier = Modifier
+                .background(color = Color.White)
+                .fillMaxWidth()
+                .clickable {
+                    uri = Uri.parse("https://file-examples.com/storage/fedf16213165ce2d096e19a/2017/04/file_example_MP4_480_1_5MG.mp4")
+                }
+                .padding(16.dp)
+            )
+            Text("Control visible: $controlVisible", modifier = Modifier
+                .background(color = Color.White)
+                .fillMaxWidth()
+                .clickable {
+                    controlVisible = !controlVisible
+                }
+            )
+        }
+    }
+
+
+}
